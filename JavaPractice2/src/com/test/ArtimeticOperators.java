0c37aa1e6af21e78ae4be6c3d462012977bb10a4
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:16:34 -0500

    java

---
+++ JavaPractice2/src/com/test/ArtimeticOperators.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class ArtimeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 4;
+		System.out.println("a * b = "  +( a * b));
+
+	}
+
+}
