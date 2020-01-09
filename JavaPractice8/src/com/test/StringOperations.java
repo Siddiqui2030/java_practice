2993e2498c157d0617bd2762dcd0b89562b77ab9
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:40:07 -0500

    JavaPractice

---
+++ JavaPractice8/src/com/test/StringOperations.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class StringOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "welcome";
+		String y = "to";
+		String z = "chicago ";
+
+		System.out.println(x + " " + y + " "+ z);
+
+	}
+
+}
