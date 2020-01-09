5993558b37eff3bb5354af3cf7758fa7b2efe820
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:31:36 -0500

    Javapractice

---
+++ JavaPractice5/StringExample.java
@@ -0,0 +1,14 @@
+
+public class Stringexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String x = "Thank", y = "You"; 
+		
+		System.out.println("x + y = " + x + y);
+		
+
+	}
+
+}
