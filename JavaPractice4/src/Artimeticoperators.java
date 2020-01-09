31075aa303c00f938d0049bd01fec3381abb66a4
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:29:08 -0500

    Javapractice

---
+++ JavaPractice4/src/Artimeticoperators.java
@@ -0,0 +1,14 @@
+
+public class Artimeticoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 45;
+		int b = 100;
+		
+		System.out.println("a % b = " + (a % b ));
+
+	}
+
+}
