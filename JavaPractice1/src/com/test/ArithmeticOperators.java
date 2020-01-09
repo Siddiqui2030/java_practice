7877e49bb48bb90156198eefdb27e212364ef647
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:13:08 -0500

    I created to Arthimetic Operators

---
+++ JavaPractice1/src/com/test/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 50; 
+		int b = 30;
+		System.out.println("a - b = " +(a - b));
+
+	}
+
+}
