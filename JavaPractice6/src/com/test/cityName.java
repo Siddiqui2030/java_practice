d912aa1473bedcb6a7aab24fc92e12c7acd803ac
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:33:17 -0500

    javapractice

---
+++ JavaPractice6/src/com/test/cityName.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class cityName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String x = "Thank", y = "You"; 
+		
+		System.out.println("x + y = " + x + y);
+
+	}
+
+}
