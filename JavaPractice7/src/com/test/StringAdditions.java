585a18b6ad1cb47d5a1f191ce3583198345a41fe
dgt30 <dgt30@10.1.10.51>
2020-01-09 16:39:00 -0500

    javapractice

---
+++ JavaPractice7/src/com/test/StringAdditions.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class StringAdditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String x = "Hello ", y = "World"; 
+		
+		System.out.println("x + y = " + x + y);
+
+
+	}
+
+}
